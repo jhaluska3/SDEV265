ed38e9e0388bcdb9d2c4041d8591c8c582438f15
jhaluska3 <jakobhaluska@outlook.com>
2023-12-16 15:44:19 -0600

    schedule db

---
+++ Final/dbo.Schedule__.data.sql
@@ -0,0 +1,6 @@
+INSERT INTO [dbo].[Schedule
+] ([Id], [Monday], [Tuesday], [Wednesday], [Thursday], [Friday], [Saturday], [Sunday]) VALUES (1, N'0', N'0', N'9-5', N'9-5', N'9-5', N'9-5', N'9-5')
+INSERT INTO [dbo].[Schedule
+] ([Id], [Monday], [Tuesday], [Wednesday], [Thursday], [Friday], [Saturday], [Sunday]) VALUES (2, N'9-5', N'9-5', N'9-5', N'9-5', N'9-5', N'0', N'0')
+INSERT INTO [dbo].[Schedule
+] ([Id], [Monday], [Tuesday], [Wednesday], [Thursday], [Friday], [Saturday], [Sunday]) VALUES (4, NULL, NULL, NULL, NULL, NULL, NULL, NULL)
